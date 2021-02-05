f9b9d727b447123b4e99bab9ea62db02187e681f
EC2 Default User <ec2-user@ip-172-31-25-232.ec2.internal>
2021-02-05 13:26:39 +0000

    [Finish]3-6

--- app/controllers/todolists_controller.rb
+++ app/controllers/todolists_controller.rb
@@ -10,11 +10,21 @@ class TodolistsController < ApplicationController
   def show
   @list=List.find(params[:id])
   end
+  def edit
+    @list=List.find(params[:id])
+  end
   def index
     @lists =List.all
   end
+  def update
+    list=List.find(params[:id])
+   list.update(list_params)
+  # list.update(list_params)
+
+    redirect_to todolist_path(list.id)
+  end
   private
-  def list_param
+  def list_params
     params.require(:list).permit(:title, :body)
 end
 end
